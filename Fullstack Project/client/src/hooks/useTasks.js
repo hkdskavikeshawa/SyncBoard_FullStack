744366c002dfc7db3f99f863407264b8aa5e525b
pramod926 <idpsampath@students.nsbm.ac.lk>
2026-08-20 00:34:02 -0700

    Revert "Change Structure 404 Page"
    This reverts commit de34be02ac1cc4b4b5519957c684920702a24713.

--- Fullstack Project/client/src/hooks/useTasks.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { TasksContext } from '../context/TasksContext';
-
-export function useTasks() {
-  const ctx = useContext(TasksContext);
-  if (!ctx) throw new Error('useTasks must be used inside <TasksProvider>');
-  return ctx;
-}
